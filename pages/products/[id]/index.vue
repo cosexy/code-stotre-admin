62dc4709a615fb8a476c1f7ad774a51d7e2a259e
Guen <heloo@guen.dev>
2023-04-17 13:32:51 +0700

    init

--- pages/products/[id]/index.vue
+++ pages/products/[id]/index.vue
@@ -4,6 +4,21 @@
       Madara - Wordpress Theme for Manga
     </includes-teleport>
 
+    <includes-teleport to="#actions">
+      <a-popconfirm
+        title="Are you sure delete this post?"
+        ok-text="Yes"
+        cancel-text="No"
+      >
+        <a-button type="danger" shape="round">
+          DELETE
+        </a-button>
+      </a-popconfirm>
+      <a-button type="primary" shape="round" class="ml-3">
+        EDIT
+      </a-button>
+    </includes-teleport>
+
     <home-counter />
     <home-revenue class="mt-8" />
   </div>
